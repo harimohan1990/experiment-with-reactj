b522b35aaafbe80331b796a6e24e658f94cff969
harimohan <harimohan.info@gmail.com>
2023-10-24 21:10:32 +0530

    google map intergraton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import CricketScoreChart from './component/CricketScoreChart';
+
+import Map from './component/MapwithReact/Map';
 
 function App() {
-  const cricketScores = [50, 60, 45, 70, 80, 55, 90];
+
 
   return (
     <div className="App">
-      <h1>Cricket Score Chart</h1>
-      <CricketScoreChart data={cricketScores} />
+      <h1>Map with Reactjs </h1>
+      <Map/>
     </div>
   );
 }
